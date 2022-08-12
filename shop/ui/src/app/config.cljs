660ea77a1adefdb897282bddb9198ebbceb97708
Rodrigo Mantica <manticarodrigo@gmail.com>
2022-08-12 00:10:45 -0600

    Fixed env vars. Switched to tailwind cli

--- shop/ui/src/app/config.cljs
+++ shop/ui/src/app/config.cljs
@@ -1,13 +1,9 @@
-(ns app.config
-  (:require-macros [adzerk.env]))
+(ns app.config)
 
 (def debug?
   ^boolean goog.DEBUG)
 
-(declare GOOGLE_MAPS_API_KEY)
-
-(adzerk.env/def
-  GOOGLE_MAPS_API_KEY (System/getenv "GOOGLE_MAPS_API_KEY"))
+(goog-define GOOGLE_MAPS_API_KEY "")
 
 (def env
   {:google-maps-api-key GOOGLE_MAPS_API_KEY})
