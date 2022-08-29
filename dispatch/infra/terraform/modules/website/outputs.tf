3d1b7df7f5b3773466d2b55d32731d589b6d7525
Rodrigo Mantica <manticarodrigo@gmail.com>
2022-08-29 09:00:38 -0600

    Added dynamic website_distribution_id

--- dispatch/infra/terraform/modules/website/outputs.tf
+++ dispatch/infra/terraform/modules/website/outputs.tf
@@ -1,12 +1,12 @@
 output "distribution_id" {
-  value = "${aws_cloudfront_distribution.s3_dist.id}"
+  value = aws_cloudfront_distribution.s3_dist.id
 }
 output "domain_name" {
-  value = "${aws_cloudfront_distribution.s3_dist.domain_name}"
+  value = aws_cloudfront_distribution.s3_dist.domain_name
 }
 output "zone_id" {
-  value = "${aws_cloudfront_distribution.s3_dist.hosted_zone_id}"
+  value = aws_cloudfront_distribution.s3_dist.hosted_zone_id
 }
 output "bucket_name" {
-  value = "${aws_s3_bucket.site_bucket.id}"
+  value = aws_s3_bucket.site_bucket.id
 }
--- dispatch/infra/terraform/outputs.tf
+++ dispatch/infra/terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "website_bucket_name" {
   value = module.website.bucket_name
 }
+output "website_distribution_id" {
+  value = module.website.distribution_id
+}
