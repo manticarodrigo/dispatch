f24cabc370ccf8944fcd06ae70aa40a57b482b3c
Rodrigo Mantica <manticarodrigo@gmail.com>
2022-10-30 23:36:05 -0600

    Invalidate all site paths

--- infra/site/main.tf
+++ infra/site/main.tf
@@ -62,7 +62,7 @@ resource "null_resource" "site_sync" {
   provisioner "local-exec" {
     command     = <<-EOT
                  aws s3 sync dispatch/public s3://${aws_s3_bucket.site_bucket.id}
-                 aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.site_s3_dist.id} --paths "/index.html,/css/main.css"
+                 aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.site_s3_dist.id} --paths "/*"
                 EOT
     working_dir = "../"
   }
