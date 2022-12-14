c201a6c414c8594df2080bbbbcaf2318604f2343
Joan Mendoza <joanmendoz3@gmail.com>
2022-12-14 14:45:23 -0600

    Change Name

--- dispatch/src/tests/stop.cljs
+++ dispatch/src/tests/stop.cljs
@@ -5,7 +5,7 @@
    [tests.util.api :refer (send)]))
 
 (defn create-arrived-at [stop-id]
-  (p/let [query (inline "mutations/stop/create-arrived-at.graphql")
+  (p/let [query (inline "mutations/stop/create-stop-arrival.graphql")
           variables {:stopId stop-id}
           request  {:query query :variables variables}
           result (send request)]
