b85c2ed5971afa88afef61a26725da2c5531a4ab
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-01-13 08:48:12 -0600

    Move rum to lib folder

--- dispatch/src/ui/core.cljs
+++ dispatch/src/ui/core.cljs
@@ -1,32 +1,15 @@
 (ns ui.core
   (:require
    ["react-dom/client" :refer (createRoot)]
-   ["aws-rum-web" :refer (AwsRum)]
    [goog.dom :as gdom]
    [reagent.core :as r]
    [re-frame.core :as rf]
-   [ui.config :as config]
    [ui.events :as events]
+   [ui.lib.aws.rum]
    [ui.lib.router :refer [browser-router]]
    [ui.lib.apollo :refer (apollo-provider)]
    [ui.components.main :refer (main)]
    [ui.components.routes :refer (routes)]))
-
-(when config/RUM_MONITOR_ID
-  (AwsRum.
-   config/RUM_MONITOR_ID
-   config/VERSION
-   "us-east-1"
-   #js{:sessionSampleRate 1
-       :endpoint "https://dataplane.rum.us-east-1.amazonaws.com"
-       :guestRoleArn config/RUM_GUEST_ROLE_ARN
-       :identityPoolId config/RUM_IDENTITY_POOL_ID
-       :telemetries #js["errors"
-                        "performance"
-                        #js["http" #js{:urlsToInclude #js[(re-pattern config/API_URL)]
-                                       :addXRayTraceIdHeader true}]]
-       :allowCookies true
-       :enableXRay true}))
 
 (def functional-compiler (r/create-compiler {:function-components true}))
 (r/set-default-compiler! functional-compiler)
