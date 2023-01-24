2707bd9b845704b9bb651f6fd6730c14a680c6be
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-01-24 10:42:10 -0600

    Added privacy policy

--- ambito/src/ui/core.cljs
+++ ambito/src/ui/core.cljs
@@ -3,16 +3,18 @@
    ["react-dom/client" :refer (createRoot)]
    [goog.dom :as gdom]
    [reagent.core :as r]
-   [ui.lib.router :refer [browser-router]]
-   [ui.components.icons.ambito :refer [ambito]]))
+   [ui.lib.router :refer [browser-router routes]]
+   [ui.views.privacy :as privacy]
+   [ui.views.home :as home]))
 
 (def functional-compiler (r/create-compiler {:function-components true}))
 (r/set-default-compiler! functional-compiler)
 
 (defn app []
   [browser-router
-   [:div {:class "flex justify-center items-center w-full h-full"}
-    [:div {:class "flex items-center"} [ambito] [:h1 {:class "ml-2 text-2xl"} "Ambito"]]]])
+   [routes
+    ["/privacy" [privacy/view]]
+    ["*" [home/view]]]])
 
 (defonce !root (atom nil))
 
--- ambito/src/ui/lib/router.cljs
+++ ambito/src/ui/lib/router.cljs
@@ -1,8 +1,18 @@
 (ns ui.lib.router
   (:require
    [react-router-dom
-    :refer (BrowserRouter)]))
+    :refer (BrowserRouter
+            Routes
+            Route)]
+   [reagent.core :as r]))
 
 (defn browser-router [& children]
   [:> BrowserRouter
    (into [:<>] children)])
+
+(defn routes [& routes]
+  [:> Routes
+   (for [[path hiccup] routes]
+     [:> Route {:key path
+                :path path
+                :element (r/as-element hiccup)}])])
