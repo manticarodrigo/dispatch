4c9b9bf3b2e911bf0dc472e594e1a42f4641c7a0
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-02-06 11:42:30 -0600

    Added seat location tracking

--- dispatch/src/ui/views/seat/layout.cljs
+++ dispatch/src/ui/views/seat/layout.cljs
@@ -1,14 +1,17 @@
 (ns ui.views.seat.layout
-  (:require ["@apollo/client" :refer (gql)]
+  (:require [react]
             [shadow.resource :refer (inline)]
             [re-frame.core :refer (dispatch)]
             [ui.subs :refer (listen)]
-            [ui.lib.apollo :refer (use-mutation)]
+            [ui.lib.apollo :refer (gql use-mutation)]
             [ui.lib.router :refer (use-params)]
+            [ui.lib.google.maps.overlay :refer (update-overlay)]
+            [ui.hooks.use-location :refer (use-location)]
             [ui.components.modal :refer (modal)]
             [ui.components.inputs.button :refer (button)]))
 
 (def LINK_DEVICE (gql (inline "mutations/device/link.graphql")))
+(def CREATE_LOCATION (gql (inline "mutations/location/create.graphql")))
 
 (defn layout [& children]
   (let [{seat-id :seat} (use-params)
@@ -16,9 +19,23 @@
         device-id (:id device)
         device-info (:info device)
         device-error (:error device)
-        [link-device] (use-mutation LINK_DEVICE {:refetchQueries ["SeatByDevice"]})
         unlinked? (= "device-not-linked" device-error)
-        invalid? (= "invalid-token" device-error)]
+        invalid? (= "invalid-token" device-error)
+        [link-device] (use-mutation LINK_DEVICE {:refetchQueries ["SeatByDevice"]})
+        watch-location (use-location)
+        [create-location] (use-mutation CREATE_LOCATION {})]
+
+    (react/useEffect
+     (fn []
+       (watch-location
+        (fn [location]
+          (let [lat-lng {:lat (:latitude location)
+                         :lng (:longitude location)}]
+            (update-overlay lat-lng)
+            (create-location {:variables (merge {:seatId seat-id} lat-lng)}))))
+       #())
+     #js[])
+
     [:<>
      [modal {:show invalid? :title "Another device already linked" :on-close #()}
       [:p {:class "mb-4"} "Looks like this seat has a device linked to it already. If you would like to link your device to this seat, please reach out to an admin and ask them to unlink the other device first."]]
