e7ffdcb69501d634a29ec6208bed5bd9f1b19d4e
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-02-08 22:52:38 -0600

    Remove moved blocks

--- infra/main.tf
+++ infra/main.tf
@@ -37,30 +37,3 @@ module "dispatch" {
   source      = "../dispatch/infra"
   domain_name = local.domain_name
 }
-
-# TODO: remove once environments are in sync
-
-moved {
-  from = module.api
-  to   = module.dispatch.module.api
-}
-
-moved {
-  from = module.build
-  to   = module.dispatch.module.build
-}
-
-moved {
-  from = module.db
-  to   = module.dispatch.module.db
-}
-
-moved {
-  from = module.site
-  to   = module.dispatch.module.site
-}
-
-moved {
-  from = module.vpc
-  to   = module.dispatch.module.vpc
-}
