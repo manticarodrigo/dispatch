ea2212639ab658c871bfd055cbbebcdde60719e2
Rodrigo Mantica <manticarodrigo@gmail.com>
2023-03-11 12:49:26 -0600

    Move place form ratoms to global scope.

--- dispatch/src/ui/components/forms/place.cljs
+++ dispatch/src/ui/components/forms/place.cljs
@@ -21,117 +21,117 @@
 
 (def CREATE_PLACE (gql (inline "mutations/place/create.graphql")))
 
+(def !state (r/atom {}))
+(def !anoms (r/atom {}))
+(def !options (r/atom []))
+(def !loading-place (r/atom false))
+(def !loading-location (r/atom false))
+
 (defn place-form []
-  (let [!state (r/atom {})
-        !anoms (r/atom {})
-        !options (r/atom [])
-        !loading-place (r/atom false)
-        !loading-location (r/atom false)]
-    (fn []
-      (let [[create status] (use-mutation CREATE_PLACE {})
-            {:keys [name description lat lng place_id phone email]} @!state
-            loading (or
-                     (:loading status)
-                     @!loading-place
-                     @!loading-location)
-            navigate (use-navigate)
-            selected-place-missing? (and
-                                     place_id
-                                     (not-any?
-                                      #(= (:place_id %) place_id)
-                                      @!options))
-            place-options (if selected-place-missing?
-                            (cons @!state @!options)
-                            @!options)]
+  (let [[create status] (use-mutation CREATE_PLACE {})
+        {:keys [name description lat lng place_id phone email]} @!state
+        loading (or
+                 (:loading status)
+                 @!loading-place
+                 @!loading-location)
+        navigate (use-navigate)
+        selected-place-missing? (and
+                                 place_id
+                                 (not-any?
+                                  #(= (:place_id %) place_id)
+                                  @!options))
+        place-options (if selected-place-missing?
+                        (cons @!state @!options)
+                        @!options)]
 
-        (react/useEffect
-         (fn []
-           (dispatch [:map
-                      {:points
-                       (when (and lat lng)
-                         [{:title (or name "???")
-                           :position {:lat lat :lng lng}}])}])
-           #())
-         #js[name lat lng])
+    (react/useEffect
+     (fn []
+       (dispatch [:map
+                  {:points
+                   (when (and lat lng)
+                     [{:title (or name "???")
+                       :position {:lat lat :lng lng}}])}])
+       #())
+     #js[name lat lng])
 
-        [:form {:class "flex flex-col"
-                :on-submit (fn [e]
-                             (.preventDefault e)
-                             (let [variables (dissoc @!state :place_id)]
-                               (-> (create {:variables variables})
-                                   (.then (fn [] (navigate "../places")))
-                                   (.catch #(reset! !anoms (parse-anoms %))))))}
-         [input {:label (tr [:field/name])
-                 :value name
-                 :required true
-                 :class "mb-4"
-                 :on-text #(swap! !state assoc :name %)}]
-         [combobox {:label (tr [:field/location-search])
-                    :class "mb-4"
-                    :options place-options
-                    :option-to-label #(:description %)
-                    :option-to-value #(:place_id %)
-                    :on-text (fn [text]
-                               (when (seq text)
-                                 (-> (search-places text)
-                                     (.then #(reset! !options %)))))
-                    :on-select (fn [option]
-                                 (reset! !state
-                                         (merge {:name name}
-                                                (select-keys option [:place_id :description])))
-                                 (reset! !loading-place true)
-                                 (-> (find-place (:place_id option))
-                                     (.then #(merge % option))
-                                     (.then #(select-keys % [:place_id :description :lat :lng]))
-                                     (.then #(swap! !state merge %))
-                                     (.then #(reset! !loading-place false))
-                                     (.catch #(reset! !loading-place false))))}]
-         [loading-button
-          {:loading @!loading-location
-           :type "button"
-           :label (tr [:field/location-get])
-           :class "mb-4"
-           :on-click (fn []
-                       (reset! !loading-location true)
-                       (-> (get-location)
-                           (.then (fn [position]
-                                    (let [lat-lng (position-to-lat-lng position)]
-                                      (swap! !state merge lat-lng)
-                                      (-> (reverse-geocode lat-lng)
-                                          (.then #(-> % :results first))
-                                          (.then #(select-keys % [:place_id :formatted_address]))
-                                          (.then #(rename-keys % {:formatted_address :description}))
-                                          (.then #(swap! !state merge %))
-                                          (.then #(reset! !loading-location false))))))
-                           (.catch #(reset! !loading-location false))))}]
-         [input {:label (tr [:field/description])
-                 :value description
-                 :required true
-                 :class "mb-4"
-                 :on-text #(swap! !state assoc :description %)}]
-         [input {:label (tr [:field/latitude])
-                 :value lat
-                 :required true
-                 :class "mb-4"
-                 :on-validate latitude?
-                 :on-text #(swap! !state assoc :lat %)}]
-         [input {:label (tr [:field/longitude])
-                 :value lng
-                 :required true
-                 :class "mb-4"
-                 :on-validate longitude?
-                 :on-text #(swap! !state assoc :lng %)}]
-         [input {:type "tel"
-                 :label (tr [:field/phone])
-                 :value phone
-                 :required false
-                 :class "mb-4"
-                 :on-text #(swap! !state assoc :phone %)}]
-         [input {:type "email"
-                 :label (tr [:field/email])
-                 :value email
-                 :required false
-                 :class "mb-4"
-                 :on-text #(swap! !state assoc :email %)}]
-         [submit-button {:loading loading}]
-         [errors @!anoms]]))))
+    [:form {:class "flex flex-col"
+            :on-submit (fn [e]
+                         (.preventDefault e)
+                         (let [variables (dissoc @!state :place_id)]
+                           (-> (create {:variables variables})
+                               (.then (fn [] (navigate "../places")))
+                               (.catch #(reset! !anoms (parse-anoms %))))))}
+     [input {:label (tr [:field/name])
+             :value name
+             :required true
+             :class "mb-4"
+             :on-text #(swap! !state assoc :name %)}]
+     [combobox {:label (tr [:field/location-search])
+                :class "mb-4"
+                :options place-options
+                :option-to-label #(:description %)
+                :option-to-value #(:place_id %)
+                :on-text (fn [text]
+                           (when (seq text)
+                             (-> (search-places text)
+                                 (.then #(reset! !options %)))))
+                :on-select (fn [option]
+                             (reset! !state
+                                     (merge {:name name}
+                                            (select-keys option [:place_id :description])))
+                             (reset! !loading-place true)
+                             (-> (find-place (:place_id option))
+                                 (.then #(merge % option))
+                                 (.then #(select-keys % [:place_id :description :lat :lng]))
+                                 (.then #(swap! !state merge %))
+                                 (.then #(reset! !loading-place false))
+                                 (.catch #(reset! !loading-place false))))}]
+     [loading-button
+      {:loading @!loading-location
+       :type "button"
+       :label (tr [:field/location-get])
+       :class "mb-4"
+       :on-click (fn []
+                   (reset! !loading-location true)
+                   (-> (get-location)
+                       (.then (fn [position]
+                                (let [lat-lng (position-to-lat-lng position)]
+                                  (swap! !state merge lat-lng)
+                                  (-> (reverse-geocode lat-lng)
+                                      (.then #(-> % :results first))
+                                      (.then #(select-keys % [:place_id :formatted_address]))
+                                      (.then #(rename-keys % {:formatted_address :description}))
+                                      (.then #(swap! !state merge %))
+                                      (.then #(reset! !loading-location false))))))
+                       (.catch #(reset! !loading-location false))))}]
+     [input {:label (tr [:field/description])
+             :value description
+             :required true
+             :class "mb-4"
+             :on-text #(swap! !state assoc :description %)}]
+     [input {:label (tr [:field/latitude])
+             :value lat
+             :required true
+             :class "mb-4"
+             :on-validate latitude?
+             :on-text #(swap! !state assoc :lat %)}]
+     [input {:label (tr [:field/longitude])
+             :value lng
+             :required true
+             :class "mb-4"
+             :on-validate longitude?
+             :on-text #(swap! !state assoc :lng %)}]
+     [input {:type "tel"
+             :label (tr [:field/phone])
+             :value phone
+             :required false
+             :class "mb-4"
+             :on-text #(swap! !state assoc :phone %)}]
+     [input {:type "email"
+             :label (tr [:field/email])
+             :value email
+             :required false
+             :class "mb-4"
+             :on-text #(swap! !state assoc :email %)}]
+     [submit-button {:loading loading}]
+     [errors @!anoms]]))
