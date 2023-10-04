b1a8f75b4c4beef1457694aebae69eabc55fa5f2
Your Name <manticarodrigo@gmail.com>
2023-10-04 08:44:20 -0600

    Comment capacitor conf

--- cljs/capacitor.config.ts
+++ cljs/capacitor.config.ts
@@ -1,30 +1,30 @@
-import { CapacitorConfig } from "@capacitor/cli";
+// import { CapacitorConfig } from "@capacitor/cli";
 
-const appIdMap = {
-  prod: "app.ambito.dispatch",
-  dev: "dev.ambito.dispatch",
-};
-const hostnameMap = {
-  prod: "dispatch.ambito.app",
-  dev: "dispatch.ambito.dev",
-};
+// const appIdMap = {
+//   prod: "app.ambito.dispatch",
+//   dev: "dev.ambito.dispatch",
+// };
+// const hostnameMap = {
+//   prod: "dispatch.ambito.app",
+//   dev: "dispatch.ambito.dev",
+// };
 
-const config: CapacitorConfig = {
-  appId: appIdMap[process.env.STAGE],
-  appName: "Dispatch",
-  webDir: "public",
-  bundledWebRuntime: false,
-  server: {
-    url: process.env.RELOAD_URL,
-    cleartext: process.env.RELOAD_URL && true,
-    hostname: hostnameMap[process.env.STAGE],
-    androidScheme: "https",
-  },
-  plugins: {
-    CapacitorUpdater: {
-      updateUrl: process.env.API_URL && `${process.env.API_URL}/update`,
-    },
-  },
-};
+// const config: CapacitorConfig = {
+//   appId: appIdMap[process.env.STAGE],
+//   appName: "Dispatch",
+//   webDir: "public",
+//   bundledWebRuntime: false,
+//   server: {
+//     url: process.env.RELOAD_URL,
+//     cleartext: process.env.RELOAD_URL && true,
+//     hostname: hostnameMap[process.env.STAGE],
+//     androidScheme: "https",
+//   },
+//   plugins: {
+//     CapacitorUpdater: {
+//       updateUrl: process.env.API_URL && `${process.env.API_URL}/update`,
+//     },
+//   },
+// };
 
-export default config;
+// export default config;
