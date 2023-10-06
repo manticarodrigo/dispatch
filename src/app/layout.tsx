36481cf40aeec0db385623aa5d13b4467e70b682
Your Name <manticarodrigo@gmail.com>
2023-10-06 00:07:20 -0600

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         <div id="map-bench">
           <div id="map-container" className="w-full h-full"></div>
         </div>
+        <Analytics />
       </body>
     </html>
   );
